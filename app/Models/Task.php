e6247e0a33904ffa9363be7b992173ce4d8afb22
Almas Sinev <iildarado26@gmail.com>
2024-03-21 13:22:44 +0300

    Tasks Model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public static function completed() 
+    {
+        return static::where('completed', 1)->get();
+    }
 }
--- database/migrations/2024_03_21_074918_create_tasks_table.php
+++ database/migrations/2024_03_21_074918_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->text('body');
-            $table->integer('user_id');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
