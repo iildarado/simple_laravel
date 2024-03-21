5d67dd567c7ae17a95d91c2bdb2d0554c4c9ba31
Almas Sinev <iildarado26@gmail.com>
2024-03-21 10:36:08 +0300

    first route and page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name = 'Almas';
+    $tasks = [
+        'Просмотреть весь курс',
+        'Прочитать книгу',
+        'Написать свой проект'
+    ];
+
+    return view('welcome', ['name'=>$name, 'tasks'=>$tasks]);
+
 });
